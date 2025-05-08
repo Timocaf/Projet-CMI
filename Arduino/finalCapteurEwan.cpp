78f1b3c38414e22a3eb743c20966f07a5ec717cc
Haro <160977429+EwanHaro@users.noreply.github.com>
2025-05-08 16:18:39 +0200

    Update finalCapteurEwan.cpp

--- Arduino/finalCapteurEwan.cpp
+++ Arduino/finalCapteurEwan.cpp
@@ -3,6 +3,29 @@
 #include <Wire.h>
 #include <SharpIR.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    7
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 24
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
 
 #define RX_BT 15
 #define TX_BT 14
